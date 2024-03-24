e101f0bca343acf5321b05fce504c83cad2e3349
Mo Arvan <mo.arvan@gmail.com>
2024-03-23 20:01:55 -0500

    added parallel_helper.py, update scrape_dgs.py

---
+++ src/parallel_helper.py
@@ -0,0 +1,57 @@
+import concurrent.futures
+import datetime
+import logging
+import re
+
+from tqdm import tqdm
+
+
+def run_in_parallel_io_bound(func, iterable, max_workers=None, disable=False, **kwargs):
+    """
+    Run a function in parallel on a list of arguments
+    :param disable: whether to disable the progress bar
+    :param func: function to run
+    :param iterable: list of arguments
+    :param max_workers: maximum number of workers to use
+    :param kwargs: keyword arguments to pass to the function
+    :return: list of results
+    """
+    results = []
+    number_of_entries = len(iterable)
+
+    with tqdm(total=number_of_entries, disable=disable) as progress_bar:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_dict = {
+                executor.submit(func, item, **kwargs): index
+                for index, item in enumerate(iterable)
+            }
+            for future in concurrent.futures.as_completed(future_dict):
+                results.append(future.result())
+                progress_bar.update(1)
+    return results
+
+
+def run_in_parallel_cpu_bound(func, iterable, max_workers=None, disable=False, total=None, **kwargs):
+    """
+    Run a function in parallel on a list of arguments
+    :param disable: whether to disable the progress bar
+    :param func: function to run
+    :param iterable: list of arguments
+    :param max_workers: maximum number of workers to use
+    :param kwargs: keyword arguments to pass to the function
+    :return: list of results
+    """
+    results = []
+    if hasattr(iterable, "len"):
+        total = len(iterable)
+
+    with tqdm(total=total, disable=disable) as progress_bar:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+            future_dict = {
+                executor.submit(func, item, **kwargs): index
+                for index, item in enumerate(iterable)
+            }
+            for future in concurrent.futures.as_completed(future_dict):
+                results.append(future.result())
+                progress_bar.update(1)
+    return results
