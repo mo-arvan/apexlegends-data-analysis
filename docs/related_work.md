025b4a93f5ee9c67fc30b8dc175355802f624bae
Mo Arvan <mo.arvan@gmail.com>
2024-05-08 10:59:25 -0500

    update

--- docs/related_work.md
+++ docs/related_work.md
@@ -1,7 +1,9 @@
 #### Related Work
 
+Below are some of the related work to this project:
+
+- [[S20] Economy and sustain- Apex legends weapon stats / By Trickyni](https://docs.google.com/spreadsheets/d/1IVFYu7eqe5zuqr-AC7TAYd9cQhhqiu0oSNEv9z7CZNY/edit#gid=1301530328), [Reddit Post](https://www.reddit.com/r/apexlegends/comments/1c71grg/s20_economy_and_sustain_a_spreadsheet_of/)
 - [Apex Stats Spreadsheet](https://docs.google.com/spreadsheets/d/1ipBvNmgK7-u2VJGlbNzsPvqydXhoRGM7Zxf6OBMvkpc/edit#gid=0)
 - [TTK simulator based on your weapon and estimated accuracy](https://www.reddit.com/r/CrucibleGuidebook/comments/vrfvfb/ttk_simulator_based_on_your_weapon_and_estimated/)
 - [TSM Damage Analysis](https://public.tableau.com/app/profile/connor.murphy2369/viz/TSMCompetitiveAnalysis/TSMTeamAnalysis?publish=yes), Reddit [post](https://www.reddit.com/r/CompetitiveApex/comments/mir49b/apex_legends_data_visualizations/)
 - [TrueGameData](https://www.truegamedata.com/)
-- [[S20] Economy and sustain- Apex legends weapon stats / By Trickyni](https://docs.google.com/spreadsheets/d/1IVFYu7eqe5zuqr-AC7TAYd9cQhhqiu0oSNEv9z7CZNY/edit#gid=1301530328), [Reddit Post](https://www.reddit.com/r/apexlegends/comments/1c71grg/s20_economy_and_sustain_a_spreadsheet_of/)
--- pages/2_ALGS_Ranking.py
+++ pages/2_ALGS_Ranking.py
@@ -13,7 +13,7 @@ logger.info(f"Running {__file__}")
 
 st.set_page_config(
     page_title="ALGS Fights Explorer",
-    page_icon="🎮",
+    page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -22,6 +22,78 @@ st.set_page_config(
         # 'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
+
+
+def get_player_ranking(input_df, minimum_damage):
+    higher_than_threshold_data = input_df.loc[
+        damage_events_filtered_df["damage_sum"] >= minimum_damage]
+
+    player_ranking_data = damage_events_filtered_df[["player_name", "team_name", "input", "hit_count", "damage_sum"]]
+
+    player_hit_count = player_ranking_data.groupby(["player_name", "team_name", "input"]).agg(
+        total_fights=("hit_count", "count"),
+    ).reset_index()
+
+    high_hit_count_players = player_ranking_data.loc[player_ranking_data["damage_sum"] >= minimum_damage]
+
+    high_hit_count_players_ranking = high_hit_count_players.groupby(["player_name", "team_name", "input"]).agg(
+        high_hit_count=("hit_count", "count"),
+    ).reset_index()
+
+    high_hit_count_players_ranking = high_hit_count_players_ranking.merge(player_hit_count, on=[
+        "player_name",
+        "team_name",
+        "input"
+    ], how="inner")
+
+    high_hit_count_players_ranking["high_hit_count_percentage"] = 100 * high_hit_count_players_ranking[
+        "high_hit_count"] / \
+                                                                  high_hit_count_players_ranking["total_fights"]
+
+    high_hit_count_players_ranking["high_hit_count_percentage"] = high_hit_count_players_ranking[
+        "high_hit_count_percentage"].apply(
+        lambda x: np.round(x, 2))
+    high_hit_count_players_ranking = high_hit_count_players_ranking.sort_values(by=["high_hit_count",
+                                                                                    "high_hit_count_percentage"
+                                                                                    ],
+                                                                                ascending=False)
+
+    high_hit_count_players_ranking["high_hit_count_rank"] = range(1, len(high_hit_count_players_ranking) + 1)
+
+    high_hit_count_players_ranking.rename(columns={"player_name": "Name",
+                                                   "team_name": "Team",
+                                                   "input": "Input",
+                                                   "high_hit_count_rank": "Rank",
+                                                   "high_hit_count": "Count",
+                                                   "total_fights": "Total",
+                                                   "high_hit_count_percentage": "Percentage"}, inplace=True)
+
+    return high_hit_count_players_ranking[
+        ["Rank", "Name", "Input", "Count", "Total", "Percentage"]], higher_than_threshold_data
+
+
+def get_team_ranking():
+    team_ranking_data = high_hit_count_players_ranking.groupby("team_name").agg(
+        high_hit_count=("high_hit_count", "sum"),
+        total_fights=("total_fights", "sum"),
+    ).reset_index()
+
+    team_ranking_data["high_hit_count_percentage"] = 100 * team_ranking_data["high_hit_count"] / team_ranking_data[
+        "total_fights"]
+
+    team_ranking_data["high_hit_count_percentage"] = team_ranking_data["high_hit_count_percentage"].apply(
+        lambda x: np.round(x, 2))
+
+    team_ranking_data = team_ranking_data.sort_values(by="high_hit_count", ascending=False)
+
+    team_ranking_data["rank"] = range(1, len(team_ranking_data) + 1)
+
+    team_ranking_data.rename(columns={"team_name": "Name",
+                                      "rank": "Rank",
+                                      "high_hit_count": "Count",
+                                      "total_fights": "Total",
+                                      "high_hit_count_percentage": "Percentage"}, inplace=True)
+
 
 logger.info("Loading data...")
 # with st.spinner("Loading data..."):
@@ -43,76 +115,38 @@ minimum_damage = st.sidebar.number_input("Minimum Damage Dealt",
                                          value=100,
                                          key="high_hit_count")
 
-higher_than_threshold_data = damage_events_filtered_df.loc[damage_events_filtered_df["damage_sum"] >= minimum_damage]
+hash_to_input_dict = data_helper.get_hash_to_input_dict()
 
-player_ranking_data = damage_events_filtered_df[["player_name", "team_name", "hit_count", "damage_sum"]]
+damage_events_filtered_df["input"] = damage_events_filtered_df["player_hash"].apply(
+    lambda x: hash_to_input_dict.get(x, "?"))
 
-player_hit_count = player_ranking_data.groupby(["player_name", "team_name"]).agg(
-    total_fights=("hit_count", "count"),
+filter_input_nan = damage_events_filtered_df[damage_events_filtered_df["input"].isna()]
+
+filter_input_nan_grouped = filter_input_nan.groupby(["player_name", "player_hash", "team_name"]).agg(
+    count=("player_name", "count"),
 ).reset_index()
 
-high_hit_count_players = player_ranking_data.loc[player_ranking_data["damage_sum"] >= minimum_damage]
+filter_input_nan_grouped = filter_input_nan_grouped.sort_values(by="count", ascending=False)
 
-high_hit_count_players_ranking = high_hit_count_players.groupby(["player_name", "team_name"]).agg(
-    high_hit_count=("hit_count", "count"),
-).reset_index()
+print(filter_input_nan_grouped[["player_name", "team_name", "count"]][:10])
 
-high_hit_count_players_ranking = high_hit_count_players_ranking.sort_values(by="high_hit_count", ascending=False)
-
-high_hit_count_players_ranking = high_hit_count_players_ranking.merge(player_hit_count, on=[
-    "player_name",
-    "team_name"], how="inner")
-
-high_hit_count_players_ranking["high_hit_count_percentage"] = 100 * high_hit_count_players_ranking["high_hit_count"] / \
-                                                              high_hit_count_players_ranking["total_fights"]
-
-high_hit_count_players_ranking["high_hit_count_percentage"] = high_hit_count_players_ranking[
-    "high_hit_count_percentage"].apply(
-    lambda x: np.round(x, 2))
-team_ranking_data = high_hit_count_players_ranking.groupby("team_name").agg(
-    high_hit_count=("high_hit_count", "sum"),
-    total_fights=("total_fights", "sum"),
-).reset_index()
-
-team_ranking_data["high_hit_count_percentage"] = 100 * team_ranking_data["high_hit_count"] / team_ranking_data[
-    "total_fights"]
-
-team_ranking_data["high_hit_count_percentage"] = team_ranking_data["high_hit_count_percentage"].apply(
-    lambda x: np.round(x, 2))
-
-team_ranking_data = team_ranking_data.sort_values(by="high_hit_count", ascending=False)
-
-hao_right_data = damage_events_filtered_df.loc[damage_events_filtered_df["player_name"] == "HAO Right"]
-
-high_hit_count_players_ranking["high_hit_count_rank"] = range(1, len(high_hit_count_players_ranking) + 1)
-
-high_hit_count_players_ranking.rename(columns={"player_name": "Name",
-                                               "high_hit_count_rank": "Rank",
-                                               "high_hit_count": "Count",
-                                               "total_fights": "Total",
-                                               "high_hit_count_percentage": "Percentage"}, inplace=True)
+high_hit_count_players_ranking, higher_than_threshold_data = get_player_ranking(damage_events_filtered_df,
+                                                                                minimum_damage)
 
 st.write("Player Rankings")
-st.dataframe(high_hit_count_players_ranking[
-                 ["Rank", "Name", "Count", "Total", "Percentage"]],
+st.dataframe(high_hit_count_players_ranking,
              hide_index=True, use_container_width=True)
 
-team_ranking_data["rank"] = range(1, len(team_ranking_data) + 1)
-
-team_ranking_data.rename(columns={"team_name": "Name",
-                                  "rank": "Rank",
-                                  "high_hit_count": "Count",
-                                  "total_fights": "Total",
-                                  "high_hit_count_percentage": "Percentage"}, inplace=True)
-
-st.write("Team Rankings")
-
-st.dataframe(team_ranking_data[["Rank", "Name", "Count", "Total", "Percentage"]]
-             , hide_index=True, use_container_width=True)
+#
+# st.write("Team Rankings")
+#
+# st.dataframe(team_ranking_data[["Rank", "Name", "Count", "Total", "Percentage"]]
+#              , hide_index=True, use_container_width=True)
 
 expander = st.expander(label='Raw Data')
 expander.dataframe(higher_than_threshold_data[[
     "player_name",
+    "input",
     "team_name",
     "tournament_full_name",
     "tournament_day",
