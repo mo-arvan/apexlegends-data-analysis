9aa7b813338a342070b9a54bb6028fa0ce44c751
Mo Arvan <mo.arvan@gmail.com>
2024-05-18 13:19:36 -0500

    clean up

--- pages/1_eDPS_Calculator.py
+++ pages/1_eDPS_Calculator.py
@@ -166,116 +166,12 @@ else:
     pre_selected_weapons = default_selection["weapon_name"]
 
 selected_weapons = st_helper.get_gun_filters(gun_df, include_hop_ups=True)
-#
-# guns_slice_df = gun_df
-#
-# guns_slice_df = guns_slice_df[np.logical_or(pd.isna(guns_slice_df["secondary_class"]),
-#                                             guns_slice_df["secondary_class"] == "Care Package")]
-#
-# class_name_list = guns_slice_df["class"].unique().tolist()
-# print(f"Class Name List: {class_name_list}")
-# class_name_list = sorted(class_name_list, key=lambda x: chart_config.class_filter_order_dict.get(x, 0))
-#
-# row_1_cols = st.columns([90, 10])
-# row_2_class_columns = st.columns(len(class_name_list) + 1)
-#
-# buttons = [
-#     # {"label": "👍",
-#     #  "value": "like",
-#     #  # "style": {"border-radius": "22px", "padding": "1px"}
-#     #  },
-#     # {"label": "👎",
-#     #  "value": "dislike",
-#     #  # "style": {"border-radius": "22px", "padding": "1px"}
-#     #  }
-# ]
-#
-# for class_name in class_name_list:
-#     buttons.append({"label": class_name,
-#                     "value": class_name,
-#                     # "style": {"border-radius": "22px", "padding": "1px"}
-#                     })
-
-# buttons_list = []
-# for i, class_name in enumerate(class_name_list):
-#     with row_2_class_columns[i + 1]:
-#         if pd.isna(class_name):
-#             print(f"Skipping {class_name}")
-#             continue
-#         buttons_list.append(st.button(class_name,
-#                                       key=f"class_{class_name}_button",
-#                                       help=f"Select all the weapons in the {class_name} class",
-#                                       use_container_width=True
-#                                       ))
-#
-#     if buttons_list[i]:
-#         weapons_to_add = guns_slice_df[guns_slice_df["class"] == class_name]["weapon_name"].unique().tolist()
-#         pre_selected_weapons.extend(weapons_to_add)
-#         pre_selected_weapons = list(set(pre_selected_weapons))
-
-# with row_1_cols[1]:
-#     container = st.container()
-#     with container:
-#         # st.write("Z")
-#         clear_all = st.button("Clear All",
-#                               key=f"clear_all_button",
-#                               help=f"Clear all the selected weapons",
-#                               use_container_width=True
-#                               )
-#     if clear_all:
-#         pre_selected_weapons = []
-# # with row_1_cols[0]:
-# #     st.write("Weapons:")
-# with row_1_cols[0]:
-#     pre_selected_weapons = sorted(pre_selected_weapons)
-#     selected_weapons = st.multiselect("Weapons:",
-#                                       guns_slice_df["weapon_name"].unique().tolist(),
-#                                       pre_selected_weapons,
-#                                       label_visibility="collapsed",
-#                                       key="selected_weapons")
-
-#
-# returned = st_btn_group(buttons=buttons,
-#                         key=f"class",
-#                         mode='buttons',
-#                         theme="dark",
-#                         size="small",
-#                         align='left',
-#                         gap_between_buttons=1,
-#                         merge_buttons=True,
-#                         return_value=True)
-
-
-order_dict = {
-    # "tournament_full_name": [
-    #     'Pro League - Year 4, Split 1',
-    #     'ALGS Championship - Year 3, Split 2',
-    #     'LCQ - Year 3, Split 2',
-    #     'ALGS Playoffs - Year 3, Split 2',
-    #     'Pro League - Year 3, Split 2',
-    #     'ALGS Playoffs - Year 3, Split 1',
-    #     'Pro League - Year 3, Split 1',
-    # ],
-}
-
-# st.session_state["filters"] = default_selection
 
 filters_dict = {
     "class": "Class",
     "weapon_name": "Weapons",
 }
 
-# st.multiselect("Weapons:", guns_slice_df["weapon_name"].unique().tolist(), selected_weapons, key="selected_weapons")
-misc_weapon_class_list = ["Past Care Package", "Future Reworks"]
-# class_name_list = filter(lambda x: x not in misc_weapon_class_list, class_name_list)
-
-# plot_container = st.container()
-# options_container = st.sidebar.expander(label="Configuration")
-
-mag_button_list = [{"label": name, "value": name} for name in chart_config.mag_list]
-
-selected_evo_shield = st.sidebar.selectbox('Evo Shield:', chart_config.evo_shield_dict.keys(), index=4,
-                                           key='evo_shield')
 
 selected_peek_time = st.sidebar.slider("Peek Time (ms):",
                                        min_value=500,
@@ -289,14 +185,26 @@ selected_mag = st.sidebar.selectbox('Mag (if applicable):',
                                     index=3,
                                     key='mag')
 
-selected_bolt = st.sidebar.selectbox('Shotgun Bolt (if applicable):', ['White', 'Blue', 'Purple'], index=2, key='bolt')
+selected_bolt = st.sidebar.selectbox('Shotgun Bolt (if applicable):',
+                                     ['White', 'Blue', 'Purple'],
+                                     index=2,
+                                     key='bolt')
 
 selected_health = st.sidebar.selectbox("Health",
                                        chart_config.health_values_dict.keys(),
                                        index=4,
                                        key='health')
 
-selected_helmet = st.sidebar.selectbox('Helmet:', chart_config.helmet_dict.keys(), index=0, key='helmet')
+selected_evo_shield = st.sidebar.selectbox('Evo Shield:',
+                                           chart_config.evo_shield_dict.keys(),
+                                           index=4,
+                                           key='evo_shield')
+
+
+selected_helmet = st.sidebar.selectbox('Helmet:',
+                                       chart_config.helmet_dict.keys(),
+                                       index=0,
+                                       key='helmet')
 
 selected_ability_modifier = st.sidebar.selectbox('Ability Modifier:',
                                                  chart_config.ability_modifier_list,
@@ -319,7 +227,7 @@ chart_y_axis = st.sidebar.selectbox('Y Axis:',
                                     [
                                         "eDPS",
                                         "Damage Dealt",
-                                        "Rank"
+                                        # "Rank"
                                     ],
                                     index=0,
                                     key='y_axis')
@@ -356,11 +264,11 @@ if len(selected_weapons) == 0:
     st.write(f"Select at least one weapon.")
 else:
     e_dps_plots = ttk_analyzer.get_e_dps_df(selected_weapons,
-                                               gun_df,
-                                               sniper_stocks_df,
-                                               standard_stocks_df,
-                                               fights_df,
-                                               conditions_dict)
+                                            gun_df,
+                                            sniper_stocks_df,
+                                            standard_stocks_df,
+                                            fights_df,
+                                            conditions_dict)
     altair_plot = plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis)
 
     with chart_container:
