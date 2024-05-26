bd66967d359db7b4604a18c3da7b1e3400265da1
Mo Arvan <mo.arvan@gmail.com>
2024-05-26 12:37:38 -0500

    u

--- pages/1_eDPS_Calculator.py
+++ pages/1_eDPS_Calculator.py
@@ -36,10 +36,14 @@ def plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis):
 
     if chart_x_axis == "Accuracy Quantile (%)":
         x_axis = alt.X('accuracy_quantile',
-                       axis=alt.Axis(title='Accuracy Quantile (%)'), sort=None)
+                       axis=alt.Axis(title='Accuracy Quantile (%)'),
+                       sort=None)
+
     elif chart_x_axis == "Accuracy (%)":
-        x_axis = alt.X('accuracy',
-                       axis=alt.Axis(title='Accuracy (%)'), sort=None)
+        x_axis = alt.X('accuracy:Q',
+                       axis=alt.Axis(title='Accuracy (%)'),
+                       sort=None)
+
     if chart_y_axis == "eDPS":
         y_axis = alt.Y('dps',
                        axis=alt.Axis(title='eDPS'),
@@ -48,18 +52,33 @@ def plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis):
         y_axis = alt.Y('damage_dealt',
                        axis=alt.Axis(title='Damage Dealt'),
                        scale=alt.Scale(zero=False))
+    elif chart_y_axis == "Uncapped eDPS":
+        y_axis = alt.Y('uncapped_dps',
+                       axis=alt.Axis(title='Uncapped eDPS'),
+                       scale=alt.Scale(zero=False))
+    elif chart_y_axis == "Uncapped Damage Dealt":
+        y_axis = alt.Y('uncapped_damage_dealt',
+                       axis=alt.Axis(title='Uncapped Damage Dealt'),
+                       scale=alt.Scale(zero=False))
 
-    dps_line = alt.Chart(dps_df).mark_line(interpolate='step-before'
-                                           ).encode(
+    dps_line = alt.Chart(dps_df).mark_line(
+        interpolate='step-before',
+        strokeWidth=3,
+    ).encode(
         x=x_axis,
         y=y_axis,
-        color=alt.Color('weapon_name:N', legend=alt.Legend(title="Weapon"), scale=alt.Scale(scheme='category20')),
+        color=alt.Color('weapon_name:N',
+                        legend=alt.Legend(title="Weapon"),
+                        scale=alt.Scale(scheme='dark2')
+                        ),
         # shape=alt.Shape('weapon'),
         tooltip=alt.value(None),
         # strokeDash=alt.StrokeDash("weapon", scale=alt.Scale(domain=list(weapon_to_stroke_dash.keys()),
         #                                                     range=list(weapon_to_stroke_dash.values())),
         #                           legend=alt.Legend(title="Weapon")),
-        strokeDash=alt.StrokeDash("weapon_name", legend=alt.Legend(title="Weapon")),
+        strokeDash=alt.StrokeDash("weapon_name",
+                                  legend=alt.Legend(title="Weapon")
+                                  ),
         # strokeWidth=alt.value(3),
         # strokeDash="symbol",
     ).properties(
@@ -68,13 +87,16 @@ def plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis):
         height=750,
     )
 
-    dps_points_line = alt.Chart(dps_df).mark_point(
+    dps_points_plot = alt.Chart(dps_df).mark_point(
         size=100,
+        filled=True,
+        opacity=1,
     ).encode(
         x=x_axis,
         y=y_axis,
         shape=alt.Shape('weapon_name', legend=alt.Legend(title="Weapon")),
-        color=alt.Color('weapon_name', legend=alt.Legend(title="Weapon"), scale=alt.Scale(scheme='category20')),
+        # color=alt.Color('weapon_name', legend=alt.Legend(title="Weapon"), scale=alt.Scale(scheme='dark2')),
+        color=alt.value("white"),
         tooltip=['weapon_name',
                  alt.Tooltip('accuracy', format=",.2f"),
                  # alt.Tooltip("accuracy_quantile", format=",.2f"),
@@ -88,39 +110,57 @@ def plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis):
                  ],
     )
 
-    fig = (alt.layer(
-        dps_line, dps_points_line,  # rules,  # points, ,  # , rules,  # text selectors,
-    ).resolve_scale(
-        shape='independent',
-        color='independent',
-        strokeDash='independent',
-    ))
-    fig = fig.interactive()
-
-    plot_list = [fig,
-                 ]
-    return plot_list
-
     # Create a selection that chooses the nearest point & selects based on x-value
     nearest = alt.selection_point(nearest=True,
                                   on='mouseover',
-                                  fields=['accuracy'],
+                                  # fields=[x_axis],
+                                  encodings=['y'],
                                   empty=False)
+
     # # Transparent selectors across the chart. This is what tells us
     # # the x-value of the cursor
-    selectors = alt.Chart(effective_ttk_df).mark_point().encode(
-        x=alt.X('accuracy'),
+    selectors = alt.Chart(dps_df).mark_point().encode(
+        y=y_axis,
         tooltip=alt.value(None),
         opacity=alt.value(0),
     ).add_params(
         nearest
     )
-    #
+
+    # # Draw a rule at the location of the selection
+    rules = (alt.Chart(dps_df).mark_rule(color='gray').encode(
+        y=y_axis,
+        # tooltip=None,
+    ).transform_filter(
+        nearest
+    ))
+
     # # Draw points on the line, and highlight based on selection
-    points = line.mark_point().encode(
+    points = dps_line.mark_point().encode(
         # y=alt.Y('ttk', axis=alt.Axis(title='Effective TTK (ms)')),
         opacity=alt.condition(nearest, alt.value(1), alt.value(0)),
     )
+
+    r = dps_df[dps_df["dps"] > 90].groupby("weapon_name").agg({"accuracy": "min"}).reset_index()
+
+    # plot the text for each line with the min accuracy to achieve at least nearest point y
+    h_line_text = alt.Chart(dps_df).transform_filter(
+        nearest
+    ).transform_aggregate(
+        groupby=["weapon_name"],
+        min_x="min(accuracy)",
+
+    ).mark_text(align='left', dx=-5, dy=10).encode(
+        x=alt.value(10),
+        # y=y_axis,
+        text=alt.Text('min_x:N', format=".2f"),
+        color=alt.value("white"),
+    ).add_params(
+        nearest
+    )
+
+    #
+
     #
     # # Draw text labels near the points, and highlight based on selection
     # text = line.mark_text(align='left', dx=-5, dy=10).encode(
@@ -128,26 +168,32 @@ def plot_effective_dps(e_dps_plots, chart_x_axis, chart_y_axis):
     #     # shape=alt.Shape('weapon', legend=None),
     # )
     #
-    # # Draw a rule at the location of the selection
-    rules = alt.Chart(effective_ttk_df).mark_rule(color='gray').encode(
-        x=alt.X('accuracy'),
-        # tooltip=None,
-    ).transform_filter(
-        nearest
-    )
-    #
-    # # Put the five layers into a chart and bind the data
+
+    # fig = dps_line
     fig = (alt.layer(
-        line, selectors, dps_points_line, rules,  # points, ,  # , rules,  # text
-    ).resolve_scale(
+        dps_line,  selectors, rules, dps_points_plot, h_line_text, # points, ,  # , ,  # , rules,  # text selectors,
+    )
+    .resolve_scale(
         shape='independent',
         color='independent',
         strokeDash='independent',
-    ))
-    fig = fig.interactive()
-    # saving the plot as html file
+    )
+    )
 
-    return fig
+    # fig = alt.layer(
+    #     fig,
+    # ).resolve_scale(
+    #     shape='independent',
+    #     color='independent',
+    #     strokeDash='independent',
+    # )
+
+    fig = fig.interactive()
+
+    plot_list = [fig,
+                 ]
+
+    return plot_list
 
 
 pre_selected_weapons = []
@@ -220,14 +266,20 @@ chart_x_axis = filter_container.selectbox('X Axis:',
                                           index=0,
                                           key='x_axis')
 
+
+
 chart_y_axis = filter_container.selectbox('Y Axis:',
-                                          [
-                                              "eDPS",
-                                              "Damage Dealt",
-                                              # "Rank"
-                                          ],
-                                          index=0,
-                                          key='y_axis')
+                                                 [
+                                                        "eDPS",
+                                                        "Damage Dealt",
+                                                        "Uncapped eDPS",
+                                                        "Uncapped Damage Dealt",
+                                                 ]
+
+                                                 ,
+                                                 index=0,
+                                                 key='y_axis')
+
 #     estimation_method_list = ["Expected Value"]
 #     selected_estimation_method = st.selectbox('Estimation Method:', estimation_method_list,
 #                                               index=0,
